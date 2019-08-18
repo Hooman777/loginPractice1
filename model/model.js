9177c6a6c1f28b84bac659a8ddb187dfac5449b3
Amir <seyedhesamian@northwesternmutual.com>
2019-08-18 13:50:04 -0500

    wip

--- model/model.js
+++ model/model.js
@@ -4,21 +4,32 @@ const path = require("path");
 let db = new sqlite3.Database(path.join(__dirname, "database", "users.db"));
 
 exports.initialize = () => {
-  db.run("CREATE TABLE users (id VARCHAR(30), email VARCHAR(20));", error => {
+  db.run(
+`CREATE TABLE IF NOT EXISTS "User" (
+	"Id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"Email"	TEXT,
+	"Password"	TEXT,
+	"Name"	TEXT
+);`
+    , error => {
     if (error) console.error(error.message);
   });
 };
 
 exports.addUser = (id, email) => {
-  db.run("INSERT INTO users (id, email) VALUES (?, ?)", [id, email], error => {
+  db.run(`
+  INSERT INTO users (Id, Name, Email, Password) VALUES (?, ?, ?, ?);  
+  `, [id, "Hooman", email, "734"], error => {
     if (error) {
       return console.error(error.message);
     }
   });
 };
 
-exports.retrieveUser = (id, email) => {
-  db.get("SELECT * FROM users", [id, email], (error, row) => {
+exports.retrieveUser = (email) => {
+  db.get(`
+  SELECT (Id, Name, Email, Password) FROM users WHERE Email = ?;
+  `, [email], (error, row) => {
     if (error) {
       return console.error(error.message);
     }
